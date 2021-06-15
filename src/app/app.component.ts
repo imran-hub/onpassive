194cc42d371ce5e68fd427fbee75cf7eec9cc224
Mohammed Imran <imran@fx-pros.com>
2021-06-15 10:45:54 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'onPassiveTest';
+export class AppComponent implements OnInit {
+  formGroup: FormGroup;
+  isSubmitted = false;
+  @ViewChild('regForm') regForm: any;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.initFormFields();
+  }
+
+  initFormFields(): void {
+    this.formGroup = this.fb.group({
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(16),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(16),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      contactNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.pattern('[7-9]\\d{9}'),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(1)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(1)]],
+    }, {
+      validators: this.mustMatchValidator('password', 'confirmPassword')
+    });
+  }
+
+  get f(): any {
+    return this.formGroup.controls;
+  }
+
+  mustMatchValidator(controlName: string, matchingControlName: string): any {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.invalid) {
+      return;
+    }
+    this.isSubmitted = true;
+    const formVal = this.formGroup.value;
+    console.log(JSON.stringify(formVal));
+    console.log(formVal);
+  }
+
+  onFormReset(): void {
+    setTimeout(() => {
+      this.isSubmitted = false;
+      this.regForm.reset();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule  } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
